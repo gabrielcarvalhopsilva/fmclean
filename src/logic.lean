e5214bfc5ac8d13ff7d811a51fbc0d446185c57f
Gabriel Carvalho <gabrielcarvalhopsilva@outlook.com>
2023-10-14 19:59:52 -0300

    theorem distr_disj_conj_converse

--- src/logic.lean
+++ src/logic.lean
@@ -323,7 +323,18 @@ end
 theorem distr_disj_conj_converse :
   (P∨Q)∧(P∨R) → P∨(Q∧R)  :=
 begin
-  sorry,
+  intro h,
+  cases h with hporq hporr,
+  cases hporq with hp hq,
+  left,
+  exact hp,
+  cases hporr with hp hr,
+  left,
+  exact hp,
+  right,
+  split,
+  exact hq,
+  exact hr,
 end
 
 
